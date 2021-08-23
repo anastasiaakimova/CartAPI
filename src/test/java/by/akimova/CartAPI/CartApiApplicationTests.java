edcac4b4247f7c9c512d22ae369ebbb824f5be22
anastasiaakimova <anastasiyaaki171@gmail.com>
2021-08-23 11:05:44 +0300

    Init  project

---
+++ src/test/java/by/akimova/CartAPI/CartApiApplicationTests.java
@@ -0,0 +1,13 @@
+package by.akimova.CartAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CartApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
