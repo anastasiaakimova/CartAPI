3cdf816c242b98a30c41a72858bd38e923586b9a
anastasiaakimova <anastasiyaaki171@gmail.com>
2021-08-24 12:06:54 +0300

    Write user controller

---
+++ src/main/java/by/akimova/CartAPI/controller/UserController.java
@@ -0,0 +1,10 @@
+package by.akimova.CartAPI.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+
+}
