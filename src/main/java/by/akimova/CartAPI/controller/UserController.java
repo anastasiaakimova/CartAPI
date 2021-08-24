d2ddb69bfd084ac84255af97ed1a4c4ff9bc5332
anastasiaakimova <anastasiyaaki171@gmail.com>
2021-08-24 15:51:22 +0300

    Write register user method

--- src/main/java/by/akimova/CartAPI/controller/UserController.java
+++ src/main/java/by/akimova/CartAPI/controller/UserController.java
@@ -1,10 +1,42 @@
 package by.akimova.CartAPI.controller;
 
+import by.akimova.CartAPI.model.Role;
+import by.akimova.CartAPI.model.User;
+import by.akimova.CartAPI.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Collection;
+import java.util.Collections;
 
 @RestController
 @RequestMapping("/users")
 public class UserController {
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    @Autowired
+    public UserController(UserRepository userRepository, PasswordEncoder passwordEncoder) {
+        this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    @PostMapping
+    public ResponseEntity<?> registerUser(@RequestBody User user) {
+        User savedUser = userRepository.findByMail(user.getMail());
+        if (savedUser != null) {
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+        user.setRole(Role.USER);
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
+        userRepository.save(user);
+        return new ResponseEntity<>(user, HttpStatus.CREATED);
+    }
 
 }
