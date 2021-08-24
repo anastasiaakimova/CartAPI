8288f3754b4ae4156617804b15a1ff2cc6a8faf8
anastasiaakimova <anastasiyaaki171@gmail.com>
2021-08-24 12:43:36 +0300

    Add repository for user

---
+++ src/main/java/by/akimova/CartAPI/repository/UserRepository.java
@@ -0,0 +1,19 @@
+package by.akimova.CartAPI.repository;
+
+import by.akimova.CartAPI.model.User;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+import java.util.UUID;
+
+/**
+ * Repository for User.
+ *
+ * @author anastasiyaakimava
+ * @version 1.0
+ */
+
+public interface UserRepository extends MongoRepository<User, String> {
+    void deleteById(UUID id);
+
+    User findById(UUID id);
+}
