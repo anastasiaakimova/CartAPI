a1d74e3427b69a73a5b1e8ad8e98f14b58675f0c
anastasiaakimova <anastasiyaaki171@gmail.com>
2021-08-24 12:44:41 +0300

    Add implementation for user's service

---
+++ src/main/java/by/akimova/CartAPI/service/impl/UserServiceImpl.java
@@ -0,0 +1,57 @@
+package by.akimova.CartAPI.service.impl;
+
+import by.akimova.CartAPI.model.User;
+import by.akimova.CartAPI.repository.UserRepository;
+import by.akimova.CartAPI.service.UserService;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.UUID;
+
+/**
+ * The class is implementation of user's business logic.
+ *
+ * @author anastasiyaakimava
+ * @version 1.0
+ */
+@Service
+public class UserServiceImpl implements UserService {
+
+    private final UserRepository userRepository;
+
+    public UserServiceImpl(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public User saveUser(User user) {
+        user.setId(UUID.randomUUID());
+        return userRepository.save(user);
+    }
+
+    @Override
+    public User findById(UUID id) {
+        return userRepository.findById(id);
+    }
+
+    @Override
+    public List<User> getAllUsers() {
+        return userRepository.findAll();
+    }
+
+    @Override
+    public User updateUser(UUID id, User user) {
+        User savedUser = userRepository.findById(id);
+
+        savedUser.setName(user.getName());
+        savedUser.setMail(user.getMail());
+        savedUser.setPassword(user.getPassword());
+
+        return userRepository.save(savedUser);
+    }
+
+    @Override
+    public void deleteUserById(UUID id) {
+        userRepository.deleteById(id);
+    }
+}
