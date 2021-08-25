8aa97d760a09d6f6d864b3a2567a885726c7091f
anastasiaakimova <anastasiyaaki171@gmail.com>
2021-08-25 10:57:19 +0300

    Add permissions

--- src/main/java/by/akimova/CartAPI/model/Role.java
+++ src/main/java/by/akimova/CartAPI/model/Role.java
@@ -1,13 +1,33 @@
 package by.akimova.CartAPI.model;
 
 import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+
+import java.util.Set;
+import java.util.stream.Collectors;
 
 public enum Role implements GrantedAuthority {
-    USER,
-    ADMIN;
+    USER(Set.of(Permission.USER_READ)),
+    ADMIN(Set.of(Permission.USER_WRITE, Permission.USER_READ));
+
+    private final Set<Permission> permissions;
+
+    Role(Set<Permission> permissions) {
+        this.permissions = permissions;
+    }
+
+    public Set<Permission> getPermissions() {
+        return permissions;
+    }
 
     @Override
     public String getAuthority() {
         return name();
+    }
+
+    public Set<SimpleGrantedAuthority> getAuthorities(){
+        return getPermissions().stream()
+                .map(permission -> new SimpleGrantedAuthority(permission.getPermission()))
+                .collect(Collectors.toSet());
     }
 }
