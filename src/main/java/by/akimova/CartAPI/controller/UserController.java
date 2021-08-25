6caeb2ba704cb5e766ed08f1b922848f88403a93
anastasiaakimova <anastasiyaaki171@gmail.com>
2021-08-25 15:38:01 +0300

    Add jwt properties

--- src/main/java/by/akimova/CartAPI/controller/UserController.java
+++ src/main/java/by/akimova/CartAPI/controller/UserController.java
@@ -2,41 +2,42 @@ package by.akimova.CartAPI.controller;
 
 import by.akimova.CartAPI.model.Role;
 import by.akimova.CartAPI.model.User;
-import by.akimova.CartAPI.repository.UserRepository;
+import by.akimova.CartAPI.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
-import java.util.Collection;
-import java.util.Collections;
+import java.util.List;
 
 @RestController
 @RequestMapping("/users")
 public class UserController {
-    private final UserRepository userRepository;
+    private final UserService userService;
     private final PasswordEncoder passwordEncoder;
 
     @Autowired
-    public UserController(UserRepository userRepository, PasswordEncoder passwordEncoder) {
-        this.userRepository = userRepository;
+    public UserController(UserService userService, PasswordEncoder passwordEncoder) {
+        this.userService = userService;
         this.passwordEncoder = passwordEncoder;
     }
 
     @PostMapping
     public ResponseEntity<?> registerUser(@RequestBody User user) {
-        User savedUser = userRepository.findByMail(user.getMail());
+        User savedUser = userService.findByMail(user.getMail());
         if (savedUser != null) {
             return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
         }
         user.setRole(Role.USER);
         user.setPassword(passwordEncoder.encode(user.getPassword()));
-        userRepository.save(user);
+        userService.saveUser(user);
         return new ResponseEntity<>(user, HttpStatus.CREATED);
+    }
+
+    @GetMapping
+    ResponseEntity<List<User>> showAllUsers() {
+        return ResponseEntity.ok(userService.getAllUsers());
     }
 
 }
