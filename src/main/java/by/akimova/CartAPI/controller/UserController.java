2e6d4be3f28346187ea29b6031dd3e6425171b40
anastasiaakimova <anastasiyaaki171@gmail.com>
2021-09-01 13:04:08 +0300

    Delete unuseful request

--- src/main/java/by/akimova/CartAPI/controller/UserController.java
+++ src/main/java/by/akimova/CartAPI/controller/UserController.java
@@ -4,12 +4,10 @@ import by.akimova.CartAPI.model.User;
 import by.akimova.CartAPI.service.UserService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
-import java.util.UUID;
 
 /**
  * REST controller user connected requests.
@@ -35,18 +33,6 @@ public class UserController {
     @GetMapping
     ResponseEntity<List<User>> showAllUsers() {
         return ResponseEntity.ok(userService.getAllUsers());
-    }
-
-    /**
-     * The method shows user by id.
-     *
-     * @param id This is id to user, which you want to find.
-     * @return ResponseEntity with one user and status ok.
-     */
-    @GetMapping("/{id}")
-    ResponseEntity<?> findUserById(@PathVariable(value = "id") UUID id) {
-        User user = userService.findById(id);
-        return ResponseEntity.ok(user);
     }
 
 }
